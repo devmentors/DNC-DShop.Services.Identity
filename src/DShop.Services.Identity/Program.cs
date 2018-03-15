56732598c6333d55aa587ea642a0a1058a3c7d4a
Piotr Gankiewicz <spetz90@gmail.com>
2018-03-15 18:23:50 +0100

    Lockbox integration, updated appsettings

--- src/DShop.Services.Identity/Program.cs
+++ src/DShop.Services.Identity/Program.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore;
+﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
+using DShop.Common.Mvc;
 
 namespace DShop.Services.Identity
 {
@@ -20,6 +14,7 @@ namespace DShop.Services.Identity
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
+                .UseLockbox()
                 .Build();
     }
 }
