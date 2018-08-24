1e14ef6f06cd2db7077b16b9f1960803aa6481f9
Piotr Gankiewicz <spetz90@gmail.com>
2018-08-24 15:20:46 +0200

    Consul

--- src/DShop.Services.Identity/Startup.cs
+++ src/DShop.Services.Identity/Startup.cs
@@ -5,7 +5,9 @@ using System.Reflection;
 using System.Threading.Tasks;
 using Autofac;
 using Autofac.Extensions.DependencyInjection;
+using Consul;
 using DShop.Common.Authentication;
+using DShop.Common.Consul;
 using DShop.Common.Dispatchers;
 using DShop.Common.Mongo;
 using DShop.Common.Mvc;
@@ -23,6 +25,7 @@ namespace DShop.Services.Identity
 {
     public class Startup
     {
+        private static readonly string[] Headers = new []{"X-Operation", "X-Resource", "X-Total-Count"};
         public IConfiguration Configuration { get; }
         public IContainer Container { get; private set; }
 
@@ -34,7 +37,17 @@ namespace DShop.Services.Identity
         public IServiceProvider ConfigureServices(IServiceCollection services)
         {
             services.AddCustomMvc();
+            services.AddConsul();
             services.AddJwt();
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy", cors => 
+                        cors.AllowAnyOrigin()
+                            .AllowAnyMethod()
+                            .AllowAnyHeader()
+                            .AllowCredentials()
+                            .WithExposedHeaders(Headers));
+            });
             var builder = new ContainerBuilder();
             builder.RegisterAssemblyTypes(Assembly.GetEntryAssembly())
                     .AsImplementedInterfaces();
@@ -51,17 +64,24 @@ namespace DShop.Services.Identity
         }
 
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, 
-            IApplicationLifetime applicationLifetime)
+            IApplicationLifetime applicationLifetime, IConsulClient client)
         {
             if (env.IsDevelopment() || env.EnvironmentName == "local")
             {
                 app.UseDeveloperExceptionPage();
             }
-            app.UseAuthentication();
             app.UseErrorHandler();
+            app.UseAuthentication();
+            app.UseCors("CorsPolicy");
+            app.UseServiceId();
             app.UseMvc();
             app.UseRabbitMq();
-            applicationLifetime.ApplicationStopped.Register(() => Container.Dispose());
+            var consulServiceId = app.UseConsul();
+            applicationLifetime.ApplicationStopped.Register(() => 
+            { 
+                client.Agent.ServiceDeregister(consulServiceId); 
+                Container.Dispose(); 
+            });
         }
     }
 }
