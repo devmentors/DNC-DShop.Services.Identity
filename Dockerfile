262d3941bc2dc3725cf3723af624411942429ce6
Piotr Gankiewicz <spetz90@gmail.com>
2018-09-10 10:07:53 +0200

    Updated scripts and readme

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,7 @@
 FROM microsoft/dotnet:2.1-aspnetcore-runtime
 WORKDIR /dotnetapp
-COPY ./bin/Docker .
-ENV ASPNETCORE_URLS http://*:5002
+COPY ./src/DShop.Services.Identity/bin/docker .
+ENV ASPNETCORE_URLS http://*:5000
 ENV ASPNETCORE_ENVIRONMENT docker
+EXPOSE 5000
 ENTRYPOINT dotnet DShop.Services.Identity.dll
--- scripts/docker-publish.sh
+++ scripts/docker-publish.sh
@@ -1,19 +1,16 @@
 #!/bin/bash
-DOCKER_ENV=''
 DOCKER_TAG=''
 
 case "$TRAVIS_BRANCH" in
   "master")
-    DOCKER_ENV=production
     DOCKER_TAG=latest
     ;;
   "develop")
-    DOCKER_ENV=development
     DOCKER_TAG=dev
     ;;    
 esac
 
 docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
-docker build -f ./src/DShop.Services.Identity/Dockerfile.$DOCKER_ENV -t dshop.services.identity:$DOCKER_TAG ./src/DShop.Services.Identity
+docker build -t dshop.services.identity:$DOCKER_TAG .
 docker tag dshop.services.identity:$DOCKER_TAG $DOCKER_USERNAME/dshop.services.identity:$DOCKER_TAG
 docker push $DOCKER_USERNAME/dshop.services.identity:$DOCKER_TAG
